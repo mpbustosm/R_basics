d5674d96006fa45898cd10f8af2fc7a6b2c43a24
Maria Paula Bustos Moreno <70234004+mpbustosm@users.noreply.github.com>
2024-07-10 17:01:12 +0200

    Update and rename helloworld to helloworld.r

---
+++ helloworld.r
@@ -0,0 +1,19 @@
+# How to Print Hello world in R
+print("Hello World!")
+
+# How to install packages in R
+install.packages("GGally")
+
+# How to load packages in the script 
+library(GGally)
+
+# How to assign variables in R
+x<-1
+y<-2
+
+# How to make basic operations in R
+result <- y - x  # Substraction 
+result <- y + x  # Addition 
+
+
+print(result)
